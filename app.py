0027290956a9388437b83c4aecba2e2221209e7e
Pablo <pablorsantana@hotmail.com>
2025-05-18 23:36:01 -0300

    with '#' will be ignored, and an empty message aborts the commit.

--- app.py
+++ app.py
@@ -3,8 +3,10 @@ import streamlit as st
 # Import necessary components from utils and agents
 from utils import client, call_agent, format_markdown_output, sanitize_filename
 from agents import create_agente_assistente, create_agente_cozinheiro, create_agente_chef
+from PIL import Image
+import base64
 
-st.set_page_config(page_title="Baú de Receitas - Forja de Receitas Épicas", layout="centered") # centered or wide
+st.set_page_config(page_title="Baú de Receitas - Forja de Receitas Épicas", layout="centered", initial_sidebar_state="auto") # centered or wide
 
 # --- CSS Styling ---
 # Função para carregar o CSS
@@ -19,8 +21,50 @@ def load_css(file_name="style.css"):
 
 # Carrega o CSS
 css_completo = load_css()
+
+# --- SVGs --- #
+svg_chatbot_icon = """<svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="currentColor" viewBox="0 0 256 256"><path d="M128,24a104,104,0,1,0,104,104A104.11,104.11,0,0,0,128,24Zm0,176a72,72,0,1,1,72-72A72.08,72.08,0,0,1,128,200Zm-8-48a8,8,0,0,1,8-8,56,56,0,0,0,56-56,8,8,0,0,1,16,0,72.08,72.08,0,0,1-72,72A8,8,0,0,1,120,152Z"></path></svg>"""
+svg_compass_visual = """<svg xmlns="http://www.w3.org/2000/svg" width="64" height="64" fill="#ffcc00" viewBox="0 0 256 256"><path d="M128,24A104,104,0,1,0,232,128,104.11,104.11,0,0,0,128,24Zm51.58,57.79-32,64a4.08,4.08,0,0,1-1.79,1.79l-64,32a4,4,0,0,1-5.37-5.37l32-64a4.08,4.08,0,0,1,1.79-1.79l64-32A4,4,0,0,1,179.58,81.79Z"></path></svg>"""
+
+
 # --- Streamlit App Layout ---
 
+# =============================================================================
+# Sidebar (Barra Lateral)
+# Onde ficam as opções e informações adicionais
+# =============================================================================
+hide_streamlit_style = """
+            <style>
+                /* Hide the Streamlit header and menu */
+                header {visibility: hidden;}
+                /* Optionally, hide the footer */
+                .streamlit-footer {display: none;}
+                /* Hide your specific div class, replace class name with the one you identified */
+                .st-emotion-cache-79elbk {display: none;}
+            </style>
+            """
+st.markdown(hide_streamlit_style, unsafe_allow_html=True)
+
+# Inicia um bloco de código que será exibido na barra lateral
+with st.sidebar:
+    st.markdown("## ") # Título na sidebar") 
+    st.text("")
+
+    if st.button("Forja de Receitas Épicas", use_container_width=True, key="forja_sidebar_button"):
+        st.switch_page("app.py")
+        
+    st.write("")
+
+    if st.button("Biblioteca Arcana", use_container_width=True, key="biblioteca_sidebar_button"):
+        st.switch_page("pages/biblioteca.py")
+        
+    st.write("")
+    
+
+    if st.button("A Bússola do Impacto", use_container_width=True, key="bussola_sidebar_button"):
+        st.switch_page("pages/bussola.py")
+        
+    st.write("")
 
 st.markdown(f"<style>{css_completo}</style>", unsafe_allow_html=True)
 
